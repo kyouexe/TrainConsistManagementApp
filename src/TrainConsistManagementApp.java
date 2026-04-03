e744834e9dd79e3393f811e923056cd81d36b523
kyouexe <sarimkhan112005@gmail.com>
2026-04-03 22:32:50 +0530

    UC5 - Preserve Insertion Order of Bogies

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,36 +1,30 @@
-import java.util.LinkedList;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
         System.out.println("=====================================");
-        System.out.println("   UC4 - Maintain Ordered Bogie Consist");
+        System.out.println("   UC5 - Preserve Insertion Order of Bogies");
         System.out.println("=====================================\n");
 
-        List<String> trainConsist = new LinkedList<>();
-        trainConsist.add("Engine");
-        trainConsist.add("Sleeper");
-        trainConsist.add("AC");
-        trainConsist.add("Cargo");
-        trainConsist.add("Guard");
+        // Create LinkedHashSet
+        Set<String> formation = new LinkedHashSet<>();
 
-        System.out.println("Initial Train Consist:");
-        System.out.println(trainConsist);
-        trainConsist.add(2, "Pantry Car");
+        // Add bogies
+        formation.add("Engine");
+        formation.add("Sleeper");
+        formation.add("Cargo");
+        formation.add("Guard");
 
-        System.out.println("\nAfter Adding Pantry Car:");
-        System.out.println(trainConsist);
-        trainConsist.remove(0);
-        trainConsist.remove(trainConsist.size() - 1);
+        // Add duplicate
+        formation.add("Sleeper");
 
-        System.out.println("\nAfter Removing First and Last Bogie:");
-        System.out.println(trainConsist);
+        // Display final formation
+        System.out.println("Final Train Formation:");
+        System.out.println(formation);
 
-        System.out.println("\nFinal Ordered Train Consist:");
-        System.out.println(trainConsist);
-
-        System.out.println("\nUC4 operations completed successfully...");
+        System.out.println("\nUC5 operations completed successfully...");
     }
 }
